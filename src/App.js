6507e210e02dc6e0a5af721ccf5cf4074c9c77f4
kimmohakkarainen <30873934+kimmohakkarainen@users.noreply.github.com>
2020-12-19 20:39:17 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -15,88 +15,78 @@ import Footer from "./components/footer.js";
 
 import "./styles.css";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.dispatch(fetchState());
-  }
+function App({ fetchState, postLogout, person }) {
+  const personName =
+    person && person.fullname ? person.fullname : person ? person.email : "";
 
-  render() {
-    const personName =
-      this.props.person && this.props.person.fullname
-        ? this.props.person.fullname
-        : this.props.person
-        ? this.props.person.email
-        : "";
+  useEffect(() => {
+    fetchState();
+  }, [fetchState]);
 
-    return (
-      <div className="App">
-        {this.props.person && (
-          <Router>
-            <div>
-              {this.props.person.role === "ADMIN" && (
-                <div>
-                  <Menu personName={personName} role="ADMIN" />
-                  <Route exact path="/" component={EnterView} />
-                  <Route exact path="/billing" component={InvoiceView} />
-                  <Route
-                    exact
-                    path="/admin/rights"
-                    component={PersonAdminView}
-                  />
-                  <Route
-                    exact
-                    path="/admin/examinations"
-                    component={ExaminationAdminView}
-                  />
-                  <Route exact path="/password" component={PasswordView} />
-                  <Route
-                    exact
-                    path="/logout"
-                    render={() => {
-                      this.props.dispatch(postLogout());
-                      return <div>Logging out</div>;
-                    }}
-                  />
-                </div>
-              )}
-              {this.props.person.role === "SECRETARY" && (
-                <div>
-                  <Menu personName={personName} role="SECRETARY" />
-                  <Route exact path="/" component={EnterView} />
-                  <Route exact path="/billing" component={InvoiceView} />
-                  <Route exact path="/password" component={PasswordView} />
-                  <Route
-                    exact
-                    path="/logout"
-                    render={() => {
-                      this.props.dispatch(postLogout());
-                      return <div>Logging out</div>;
-                    }}
-                  />
-                </div>
-              )}
-              {this.props.person.role === "DOCTOR" && (
-                <div>
-                  <Menu personName={personName} role="DOCTOR" />
-                  <Route exact path="/" component={DoctorView} />
-                  <Route exact path="/password" component={PasswordView} />
-                  <Route
-                    exact
-                    path="/logout"
-                    render={() => {
-                      this.props.dispatch(postLogout());
-                      return <div>Logging out</div>;
-                    }}
-                  />
-                </div>
-              )}
-            </div>
-          </Router>
-        )}
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {person && (
+        <Router>
+          <div>
+            {person.role === "ADMIN" && (
+              <div>
+                <Menu personName={personName} role="ADMIN" />
+                <Route exact path="/" component={EnterView} />
+                <Route exact path="/billing" component={InvoiceView} />
+                <Route exact path="/admin/rights" component={PersonAdminView} />
+                <Route
+                  exact
+                  path="/admin/examinations"
+                  component={ExaminationAdminView}
+                />
+                <Route exact path="/password" component={PasswordView} />
+                <Route
+                  exact
+                  path="/logout"
+                  render={() => {
+                    postLogout();
+                    return <div>Logging out</div>;
+                  }}
+                />
+              </div>
+            )}
+            {person.role === "SECRETARY" && (
+              <div>
+                <Menu personName={personName} role="SECRETARY" />
+                <Route exact path="/" component={EnterView} />
+                <Route exact path="/billing" component={InvoiceView} />
+                <Route exact path="/password" component={PasswordView} />
+                <Route
+                  exact
+                  path="/logout"
+                  render={() => {
+                    postLogout();
+                    return <div>Logging out</div>;
+                  }}
+                />
+              </div>
+            )}
+            {person.role === "DOCTOR" && (
+              <div>
+                <Menu personName={personName} role="DOCTOR" />
+                <Route exact path="/" component={DoctorView} />
+                <Route exact path="/password" component={PasswordView} />
+                <Route
+                  exact
+                  path="/logout"
+                  render={() => {
+                    postLogout();
+                    return <div>Logging out</div>;
+                  }}
+                />
+              </div>
+            )}
+          </div>
+        </Router>
+      )}
+      <Footer />
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
@@ -105,4 +95,11 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchState: () => dispatch(fetchState()),
+    postLogout: () => dispatch(postLogout())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
